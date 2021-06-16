4c0b2aacdb8ffb84b6939c2c72e09e3bf685bcee
Ignacio Chitnisky <elinmortal@gmail.com>
2021-06-16 04:03:58 +0000

    fixing sort

--- sort.h
+++ sort.h
@@ -28,5 +28,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int part(int *array, int izq, int der, size_t size);
-void quick_recursion(int *array, int izq, int der, size_t size)
+void quick_recursion(int *array, int izq, int der, size_t size);
 #endif
